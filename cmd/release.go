c3fa088242847cab81759a6dae42f68f00aac249
Christian Gregg <christian@bissy.io>
2021-05-24 21:46:36 +0100

    release cmd description

--- cmd/release.go
+++ cmd/release.go
@@ -15,14 +15,8 @@ import (
 // TODO: don't require the slug ID, search the compile app for a release that matches the current commit.
 var releaseCmd = &cobra.Command{
 	Use:   "release [target]",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: cobra.ExactArgs(1),
+	Short: "Promotes a release from your compiler app to your target app.",
+	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		client, err := cmplr.Client()
 		if err != nil {
@@ -35,15 +29,12 @@ to quickly create a Cobra application.`,
 		}
 
 		section(os.Stdout, "Releasing %v from %v to %v", hash[:7], compileAppID, args[0])
-
-		description := fmt.Sprintf("Deploy %s", hash[:7])
-
-		section(os.Stdout, "Finding correct release...")
+		log(os.Stdout, "Finding correct release...")
 		releases, err := client.ReleaseList(context.Background(), compileAppID, nil)
 		if err != nil {
 			return err
 		}
-
+		description := fmt.Sprintf("Deploy %s", hash[:7])
 		var compileRelease *heroku.Release
 		for _, release := range releases {
 			if strings.HasPrefix(release.Description, description) {
@@ -51,10 +42,10 @@ to quickly create a Cobra application.`,
 				break
 			}
 		}
-
 		if compileRelease == nil {
 			return fmt.Errorf("could not find release on compile app for %v", hash)
 		}
+
 		log(os.Stdout, "Found release %v", compileRelease.ID)
 
 		section(os.Stdout, "Releasing slug %v to %v", compileRelease.Slug.ID, args[0])
