4e18d3f90588c8a551beebe67d114073976170ca
Christian Gregg <christian@bissy.io>
2021-09-04 11:27:37 +0100

    Package docs

---
+++ procfile/doc.go
@@ -0,0 +1,7 @@
+// package procfile allows for reading, modifying, and writing Procfile format
+// files.
+//
+// It does not preserve the ordering of definitions within a Procfile.
+//
+// See: https://devcenter.heroku.com/articles/procfile
+package procfile
