7dddb4a7abff84e673339c071d45369499d05323
Christian Gregg <christian@bissy.io>
2021-09-04 14:50:19 +0100

    Extract ReleaseCmd to slugcmplr package

--- cmd/slugcmplr/release.go
+++ cmd/slugcmplr/release.go
@@ -1,85 +1,20 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 	"time"
 
-	heroku "github.com/heroku/heroku-go/v5"
+	"github.com/cga1123/slugcmplr"
 	"github.com/spf13/cobra"
 )
 
-// Release contains the configuration required for the release subcommand.
-type Release struct {
+type release struct {
 	Application string `json:"application"`
 	Slug        string `json:"slug"`
 	Commit      string `json:"commit"`
-}
-
-func release(ctx context.Context, out outputter, h *heroku.Service, buildDir, application string) error {
-	step(out, "Reading release")
-	log(out, "From: %v", filepath.Join(buildDir, "release.json"))
-
-	rf, err := os.Open(filepath.Join(buildDir, "release.json"))
-	if err != nil {
-		return fmt.Errorf("failed to read release data: %w", err)
-	}
-	defer rf.Close() // nolint:errcheck
-
-	r := &Release{}
-	if err := json.NewDecoder(rf).Decode(r); err != nil {
-		return fmt.Errorf("failed to decode release data: %w", err)
-	}
-
-	if application == "" {
-		application = r.Application
-	}
-
-	log(out, "application: %v", application)
-	log(out, "slug: %v", r.Slug)
-
-	step(out, "Releasing slug %v to %v", r.Slug, r.Application)
-
-	release, err := h.ReleaseCreate(ctx, application, heroku.ReleaseCreateOpts{
-		Slug:        r.Slug,
-		Description: heroku.String(fmt.Sprintf("Deployed %v", r.Commit[:8])),
-	})
-	if err != nil {
-		return fmt.Errorf("error creating release: %w", err)
-	}
-
-	if release.OutputStreamURL != nil {
-		if err := outputStream(out, os.Stdout, *release.OutputStreamURL); err != nil {
-			return fmt.Errorf("failed to stream output: %w", err)
-		}
-	}
-
-	for i := 0; i < 36; i++ {
-		log(out, "checking release status... (attempt %v)", i+1)
-
-		info, err := h.ReleaseInfo(ctx, r.Application, release.ID)
-		if err != nil {
-			return fmt.Errorf("failed to fetch release info: %w", err)
-		}
-
-		log(out, "status: %v", info.Status)
-
-		switch info.Status {
-		case "failed":
-			return fmt.Errorf("release failed")
-		case "succeeded":
-			return nil
-		case "pending":
-			time.Sleep(5 * time.Second)
-
-			continue
-		}
-	}
-
-	return fmt.Errorf("release still pending after multiple attempts")
 }
 
 func releaseCmd(verbose bool) *cobra.Command {
@@ -89,13 +24,77 @@ func releaseCmd(verbose bool) *cobra.Command {
 		Short: "release a slug",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			output := outputterFromCmd(cmd, verbose)
-			client, err := netrcClient(output)
+			ctx := cmd.Context()
+			out := outputterFromCmd(cmd, verbose)
+			h, err := netrcClient(out)
 			if err != nil {
 				return err
 			}
 
-			return release(cmd.Context(), output, client, buildDir, application)
+			step(out, "Reading release")
+			log(out, "From: %v", filepath.Join(buildDir, "release.json"))
+
+			rf, err := os.Open(filepath.Join(buildDir, "release.json"))
+			if err != nil {
+				return fmt.Errorf("failed to read release data: %w", err)
+			}
+			defer rf.Close() // nolint:errcheck
+
+			r := &release{}
+			if err := json.NewDecoder(rf).Decode(r); err != nil {
+				return fmt.Errorf("failed to decode release data: %w", err)
+			}
+
+			if application == "" {
+				application = r.Application
+			}
+
+			log(out, "application: %v", application)
+			log(out, "slug: %v", r.Slug)
+
+			step(out, "Releasing slug %v to %v", r.Slug, r.Application)
+
+			releaseCmd := &slugcmplr.ReleaseCmd{
+				Heroku:      h,
+				Application: application,
+				SlugID:      r.Slug,
+				Commit:      r.Commit,
+			}
+
+			release, err := releaseCmd.Execute(ctx, out)
+			if err != nil {
+				return fmt.Errorf("error creating release: %w", err)
+			}
+
+			if release.OutputStreamURL != nil {
+				if err := outputStream(out, os.Stdout, *release.OutputStreamURL); err != nil {
+					return fmt.Errorf("failed to stream output: %w", err)
+				}
+			}
+
+			for i := 0; i < 36; i++ {
+				log(out, "checking release status... (attempt %v)", i+1)
+
+				info, err := h.ReleaseInfo(ctx, r.Application, release.ID)
+				if err != nil {
+					return fmt.Errorf("failed to fetch release info: %w", err)
+				}
+
+				log(out, "status: %v", info.Status)
+
+				switch info.Status {
+				case "failed":
+					return fmt.Errorf("release failed")
+				case "succeeded":
+					return nil
+				case "pending":
+					time.Sleep(5 * time.Second)
+
+					continue
+				}
+			}
+
+			return fmt.Errorf("release still pending after multiple attempts")
 		},
 	}
 
