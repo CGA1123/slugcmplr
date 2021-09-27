388c481dd484489c0283a7c3ed43d4a881ba1ce9
Christian Gregg <christian@bissy.io>
2021-09-28 00:18:50 +0100

    Configure OpenTelemetry OTLP exporter

--- cmd/slugcmplr/server.go
+++ cmd/slugcmplr/server.go
@@ -6,6 +6,13 @@ import (
 
 	"github.com/cga1123/slugcmplr"
 	"github.com/spf13/cobra"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
 func serverCmd(verbose bool) *cobra.Command {
@@ -28,6 +35,70 @@ func serverCmd(verbose bool) *cobra.Command {
 				return fmt.Errorf("error fetching environment: %w", err)
 			}
 
+			// OTEL OTLP exporters can be configured with the following ENV vars:
+			// - OTEL_EXPORTER_OTLP_ENDPOINT (e.g. https://api.honeycomb.io)
+			// - OTEL_EXPORTER_OTLP_HEADERS (e.g. x-honeycomb-team=<API-KEY>,x-honeycomb-dataset=<dataset>)
+			// - OTEL_EXPORTER_OTLP_COMPRESSION (e.g. gzip)
+			// - OTEL_EXPORTER_OTLP_PROTOCOL (e.g. grpc)
+			exp, err := otlptracegrpc.New(cmd.Context())
+			if err != nil {
+				return fmt.Errorf("failed to setup otelgrpc exporter: %w", err)
+			}
+
+			bsp := sdktrace.NewBatchSpanProcessor(exp)
+			tp := sdktrace.NewTracerProvider(
+				sdktrace.WithSpanProcessor(bsp),
+				sdktrace.WithResource(
+					// Default attributes for every span.
+					//
+					// The following are provided by runtime-dyno-metadata feature in Heroku
+					// See: https://devcenter.heroku.com/articles/dyno-metadata
+					// - HEROKU_APP_NAME
+					// - HEROKU_DYNO_ID
+					// - HEROKU_RELEASE_VERSION
+					// - HEROKU_SLUG_COMMIT
+					//
+					// The Heroku runtime provides the following environment variables:
+					// See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+					// - DYNO
+					//
+					// These environment variables are expected to be managed
+					// by the application owner:
+					// - SLUGCMPLR_HEROKU_ACCOUNT
+					// - SLUGCMPLR_HEROKU_STACK
+					// - SLUGCMPLR_ENV
+					resource.NewWithAttributes(
+						"https://opentelemetry.io/schemas/v1.4.0",
+
+						// Cloud keys
+						semconv.CloudProviderKey.String("heroku"),
+						semconv.CloudAccountIDKey.String(os.Getenv("SLUGCMPLR_HEROKU_ACCOUNT")),
+						semconv.CloudPlatformKey.String(os.Getenv("SLUGCMPLR_HEROKU_STACK")),
+
+						// Service keys
+						semconv.ServiceNamespaceKey.String(os.Getenv("HEROKU_APP_NAME")),
+						semconv.ServiceNameKey.String("slugcmplr-http"),
+						semconv.ServiceInstanceIDKey.String(os.Getenv("HEROKU_DYNO_ID")),
+						semconv.ServiceVersionKey.String(os.Getenv("HEROKU_RELEASE_VERSION")),
+						attribute.String("service.revision", os.Getenv("HEROKU_SLUG_COMMIT")),
+
+						// Deplyment environment
+						semconv.DeploymentEnvironmentKey.String(os.Getenv("SLUGCMPLR_ENV")),
+					),
+				),
+			)
+
+			otel.SetTracerProvider(tp)
+			propagator := propagation.NewCompositeTextMapPropagator(propagation.Baggage{}, propagation.TraceContext{})
+			otel.SetTextMapPropagator(propagator)
+
+			// Handle this error in a sensible manner where possible
+			defer func() {
+				if err := tp.Shutdown(cmd.Context()); err != nil {
+					fmt.Fprintf(output.ErrOrStderr(), "error shutting down tracing: %v", err)
+				}
+			}()
+
 			s := &slugcmplr.ServerCmd{
 				Port:        env["PORT"],
 				Environment: env["SLUGCMPLR_ENV"],
@@ -43,6 +114,7 @@ func serverCmd(verbose bool) *cobra.Command {
 func defaultEnv() error {
 	defaults := map[string]string{
 		"SLUGCMPLR_ENV": "development",
+		"PORT":          "1123",
 	}
 	for k, v := range defaults {
 		if _, ok := os.LookupEnv(k); ok {
