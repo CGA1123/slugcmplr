d8e8eecff449f04a1589de2637cf9e8701a8bb6d
Christian Gregg <christian@bissy.io>
2021-09-27 19:41:19 +0100

    Rename procfile -> processfile
    Avoid conflicts with `Procfile`

--- processfile/doc.go
+++ processfile/doc.go
@@ -1,7 +1,7 @@
-// Package procfile allows for reading, modifying, and writing Procfile format
+// Package processfile allows for reading, modifying, and writing Procfile format
 // files.
 //
 // It does not preserve the ordering of definitions within a Procfile.
 //
 // See: https://devcenter.heroku.com/articles/procfile
-package procfile
+package processfile
--- processfile/procfile.go
+++ processfile/procfile.go
@@ -1,4 +1,4 @@
-package procfile
+package processfile
 
 import (
 	"bufio"
