ce4e412d96b4ba5b0261f5956e25a7cb9e2c5f47
Christian Gregg <christian@bissy.io>
2022-12-28 14:48:02 +0000

    Linting fixes

--- cmd/slugcmplr/compile.go
+++ cmd/slugcmplr/compile.go
@@ -1,3 +1,4 @@
+// Package main is the entrypoint to the `slugcmplr` binary.
 package main
 
 import (
---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package slugcmplr exposes functions to transform a source repository to a
+// Heroku compatible tarball known as a "slug" via a set of Buildpacks.
+package slugcmplr
